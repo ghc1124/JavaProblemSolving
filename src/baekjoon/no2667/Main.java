7793ad311f4fa19624f790442871c0e47eed9872
kirin316 <ghc0409@gmail.com>
2022-05-03 01:15:25 +0900

    0502 commit

--- src/baekjoon/no2667/Main.java
+++ src/baekjoon/no2667/Main.java
@@ -3,48 +3,47 @@ package baekjoon.no2667;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
-    private static int[][] directions = {
-            { -1, 0 }, // 상
-            { 1, 0 },  // 하
-            { 0, -1 }, // 좌
-            { 0, 1 },  // 우
-    };
-
-    private static int count = 1;
+    private static int[][] directions = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }, };
+    private static int[][] map;
+    private static int N, cnt;
 
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(reader.readLine());
-        int[][] arr = new int[N][N];
-        int groupCount = 0;
-        ArrayList<Integer> list = new ArrayList<>();
 
+        N = Integer.parseInt(reader.readLine());    // 지도의 크기
+
+        map = new int[N][N];
+        String[] temp = {};
         for (int i = 0; i < N; i++) {
-            String[] temp = reader.readLine().split("");
+            temp = reader.readLine().split("");
             for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(temp[j]);
+                map[i][j] = Integer.parseInt(temp[j]);
             }
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i][j] == 1) {
-                    groupCount++;
-                    arr[i][j] = 0;
-                    dfs(arr, i, j);
-                    list.add(count);
-                    count = 1;
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1) {
+                    cnt = 0;
+                    dfs(i, j);
+                    list.add(cnt);
                 }
             }
         }
 
-        sb.append(groupCount).append("\n");
         Collections.sort(list);
+
+        sb.append(list.size()).append("\n");
+
         for (int i = 0; i < list.size(); i++) {
             sb.append(list.get(i)).append("\n");
         }
@@ -52,46 +51,18 @@ public class Main {
         System.out.println(sb);
     }
 
-    private static void dfs(int[][] arr, int x, int y) {
-        for (int i = 0; i < 4; i++) {
-            int dx = x + directions[i][0];
-            int dy = y + directions[i][1];
+    private static void dfs(int y, int x) {
+        map[y][x] = 0;
+        cnt++;
 
-            if (dx >= 0 && dx < arr.length && dy >= 0 && dy < arr.length) {
-                if (arr[dx][dy] == 1) {
-                    count++;
-                    arr[dx][dy] = 0;
-                    dfs(arr, dx, dy);
-                }
+        for (int i = 0; i < 4; i++) {
+            int dy = y + directions[i][0];
+            int dx = x + directions[i][1];
+
+            if (dy >= 0 && dy < N && dx >= 0 && dx < N) {
+                if (map[dy][dx] == 1) dfs(dy, dx);
             }
         }
     }
-
-    /*private static int bfs(int[][] arr, int[] startPoint) {
-        Queue<int[]> queue = new ArrayDeque<>(); // 배열로 표현된 좌표를 담는 큐
-        queue.offer(startPoint);
-
-        int count = 1;
-
-        while (!queue.isEmpty()) {
-            int[] point = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int dx = point[0] + directions[i][0];
-                int dy = point[1] + directions[i][1];
-
-                // 유효범위일 경우
-                if (dx >= 0 && dx < arr.length && dy >= 0 && dy < arr.length) {
-                    if (arr[dx][dy] == 1) {
-                        count++;
-                        arr[dx][dy] = 0;
-                        queue.offer(new int[] { dx, dy });
-                    }
-                }
-            }
-        }
-
-        return count;
-    }*/
 
 }
