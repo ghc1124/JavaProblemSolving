b5a53e67c5e56bea0df057c8e1d8756e41733f63
ghc1124 <ghc0409@gmail.com>
2022-08-07 22:37:53 +0900

    0807 commit

---
+++ src/baekjoon/no1100/Main.java
@@ -0,0 +1,33 @@
+package baekjoon.no1100;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int cnt = 0;
+
+        char[][] arr = new char[8][8];
+        for (int i = 0; i < 8; i++) {
+            arr[i] = reader.readLine().toCharArray();
+
+            if (i % 2 == 0) {
+                // 짝수인 경우
+                for (int j = 0; j < 8; j += 2) {
+                    if (arr[i][j] == 'F') cnt++;
+                }
+            } else {
+                for (int j = 1; j < 8; j += 2) {
+                    if (arr[i][j] == 'F') cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+}
